077a86296f8bf5ba085087ceb4eade83da697931
Juan Esteban Londoño Uribe <98909164+JuanLondono01@users.noreply.github.com>
2025-04-22 16:57:00 -0500

    Update login.controller.js

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -34,8 +34,8 @@ const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 60 * 60 * 1000,
         });
 
