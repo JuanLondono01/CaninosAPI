1fc82fd86defc2e6ce6431662846618481baae27
Juan Esteban Londoño Uribe <jelu0204@gmail.com>
2025-04-22 16:24:46 -0500

    cchanges

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   credentials: true
 }))
 
