2b7d6008fc479f58246cb74e2c1455229be32ac3
AlejandroGranada1 <alejandrogranadauio@gmail.com>
2025-04-23 08:00:37 -0500

    Update cors origins

--- src/app.js
+++ src/app.js
@@ -10,16 +10,7 @@ app.use(express.json());
 app.use(cookieParser())
 let allowedOrigins = ["https://caninosapi-production.up.railway.app", "http://localhost:5173"]
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true)
-    if(allowedOrigins.indexOf(origin) === -1){
-      let msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-
-      return callback(new Error(msg), false)
-    }
-    return callback(null, true)
-  },
+  origin: allowedOrigins,
   credentials: true
 }))
 
